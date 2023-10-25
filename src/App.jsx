24e956c1735f0ad717cd915cc815af9338ac5c62
Jawad <jawad.php@gmail.com>
2023-10-24 23:47:12 -0400

    Test component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './features/dashboard/Dashboard';
 import OAuthCallback from './features/authentication/OAuthCallback';
+import Test from './features/dashboard/Test';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/auth" element={<OAuthCallback />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/auth/fitbit" element={<OAuthCallback />} />
         <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
