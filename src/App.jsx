6bbd8be3ba115f9413ecea5f2de4c2e6244da299
Jawad <jawad.php@gmail.com>
2023-10-24 22:23:50 -0400

    revert

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline grid h-screen place-items-center">
-        Hello world of fitness!
+        Hello world!
       </h1>
     </div>
   );
