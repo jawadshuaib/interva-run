9c45cf8b5af408f44626e755f16f93db2db2ffe4
Jawad <jawad.php@gmail.com>
2023-10-24 23:23:16 -0400

    Changed fl name

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import { Provider } from 'react-redux';
 import './App.css';
 import Dashboard from './features/dashboard/Dashboard';
-import AuthCallback from './features/authentication/AuthCallback';
+import OAuthCallback from './features/authentication/OAuthCallback';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/auth/fitbit" element={<AuthCallback />} />
+        <Route path="/auth/fitbit" element={<OAuthCallback />} />
         <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
---
+++ src/features/authentication/OAuthCallback.jsx
@@ -0,0 +1,26 @@
+// AuthCallback.js
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+export default function OAuthCallback() {
+  useEffect(() => {
+    // Obtain the authorization code from the URL query parameters
+    const code = new URLSearchParams(window.location.search).get('code');
+
+    if (code) {
+      // Send a POST request to exchange the authorization code for tokens
+      axios
+        .post('/fitbit/token-exchange', { code })
+        .then((response) => {
+          // Store the access token and refresh token securely (e.g., in a state or local storage)
+          console.log(response);
+        })
+        .catch((error) => {
+          // Handle any errors
+          console.log(error);
+        });
+    }
+  }, []);
+
+  return <div>Authenticating...</div>;
+}
