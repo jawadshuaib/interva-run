651bd0e99bc45a3d3986e27c1b8a9e4ae9f46e4e
Jawad <jawad.php@gmail.com>
2023-10-25 05:20:08 -0400

    Added store

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 import Dashboard from './features/dashboard/Dashboard';
 import OAuthCallback from './features/authentication/OAuthCallback';
-import PageNotFound from './features/dashboard/PageNotFound';
+import PageNotFound from './features/pages/PageNotFound';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/auth/fitbit" element={<OAuthCallback />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/auth/fitbit" element={<OAuthCallback />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Provider>
     </Router>
   );
 }
