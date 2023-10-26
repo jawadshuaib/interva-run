7a3bf6faed0917e858284bbaebd0ac8a80d7170a
Jawad <jawad.php@gmail.com>
2023-10-25 23:01:39 -0400

    Configured websockets

--- netlify/functions/fitbit-notifications.js
+++ netlify/functions/fitbit-notifications.js
@@ -1,21 +1,100 @@
 /* eslint-env node */
+import http from 'http';
+import WebSocket from 'ws';
 
-export async function handler(event) {
-  const queryParams = event.queryStringParameters;
-  const verifyCode = queryParams.verify;
+const server = http.createServer((req, res) => {
+  // Handle HTTP POST requests for Fitbit notifications
+  if (req.method === 'POST' && req.url === '/fitbit-notifications') {
+    let data = '';
 
-  if (
-    verifyCode ===
-    '171ce6758e342278717f95a2034dde6cc4c1a584351b1be329adbe3cfd736039'
-  ) {
-    // Respond with HTTP 204 for the correct verification code
-    return {
-      statusCode: 204,
-    };
+    req.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    req.on('end', () => {
+      try {
+        const requestData = JSON.parse(data);
+
+        // Handle and process the Fitbit notifications here
+        console.log('Received Fitbit Notifications:', requestData);
+
+        // Broadcast the notification to connected WebSocket clients
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(requestData));
+          }
+        });
+
+        // Respond with an HTTP 204 No Content status code to acknowledge receipt
+        res.writeHead(204);
+        res.end();
+      } catch (error) {
+        console.error('Error processing Fitbit notification:', error);
+        res.writeHead(500);
+        res.end('Internal Server Error');
+      }
+    });
   } else {
-    // Respond with HTTP 404 for an incorrect verification code
-    return {
-      statusCode: 404,
-    };
+    // Respond with an error status code for unsupported HTTP methods or routes
+    res.writeHead(405); // Method Not Allowed
+    res.end('Method Not Allowed');
   }
-}
+});
+
+const wss = new WebSocket.Server({ server });
+
+// wss.on('connection', (ws) => {
+//   // Handle WebSocket connection if needed
+//   ws.on('message', (message) => {
+//     // Handle WebSocket messages if needed
+//   });
+// });
+
+server.listen(8080, () => {
+  console.log('Server is listening on port 8080');
+});
+
+// export async function handler(event) {
+//   if (event.httpMethod === 'POST') {
+//     const requestData = JSON.parse(event.body);
+//     // Handle and process the Fitbit notifications here
+
+//     // Broadcast the notification to connected WebSocket clients
+//     wss.clients.forEach((client) => {
+//       if (client.readyState === WebSocket.OPEN) {
+//         client.send(JSON.stringify(requestData));
+//       }
+//     });
+
+//     return {
+//       statusCode: 204,
+//     };
+//   } else {
+//     return {
+//       statusCode: 405, // Method Not Allowed
+//     };
+//   }
+// }
+
+// For verification
+// export async function handler(event) {
+//   const queryParams = event.queryStringParameters;
+//   const verifyCode = queryParams?.verify;
+
+//   if (verifyCode !== undefined) {
+//     if (
+//       verifyCode ===
+//       '171ce6758e342278717f95a2034dde6cc4c1a584351b1be329adbe3cfd736039'
+//     ) {
+//       // Respond with HTTP 204 for the correct verification code
+//       return {
+//         statusCode: 204,
+//       };
+//     } else {
+//       // Respond with HTTP 404 for an incorrect verification code
+//       return {
+//         statusCode: 404,
+//       };
+//     }
+//   }
+// }
--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -1,49 +1,84 @@
 import React, { useEffect } from 'react';
+import io from 'socket.io-client';
+
 // import { useSelector } from 'react-redux';
 import AuthenticationBtn from '../authentication/AuthenticationBtn';
 
 export default function Dashboard() {
   const accessToken = localStorage.getItem('accessToken');
 
+  const [notifications, setNotifications] = React.useState([]);
+
   useEffect(() => {
-    if (accessToken === null) return;
+    // Replace with the URL of your server where WebSocket is hosted
+    const socket = io('/.netlify/functions/fitbit-notifications');
 
-    // Create a new WebSocket connection to the Fitbit Real-Time API
-    const socket = new WebSocket('wss://api.fitbit.com/1/user/-/bp/live');
+    socket.on('notification', (data) => {
+      // Handle incoming notifications
+      setNotifications((prevNotifications) => [...prevNotifications, data]);
+    });
 
-    // Set up the event handlers for the WebSocket connection
-    socket.onopen = () => {
-      // Connection is open; you can now subscribe to heart rate data.
-      // You will need to send an authorization header with your access token.
-
-      // Prepare the subscription request, including the access token
-      const subscriptionRequest = {
-        action: 'SUBSCRIBE',
-        device: 'tracker',
-        type: 'heartrate',
-        access_token: accessToken,
-      };
-
-      // Send the subscription request as a JSON string
-      socket.send(JSON.stringify(subscriptionRequest));
-    };
-
-    socket.onmessage = (event) => {
-      // Handle real-time heart rate data received in the event.data
-      const heartRateData = JSON.parse(event.data);
-      console.log('Live Heart Rate Data:', heartRateData);
-    };
-
-    socket.onclose = (event) => {
-      console.log('Socket is closed', event);
-      // Handle WebSocket closure (e.g., reconnection logic, error handling)
-    };
-
+    // Clean up the WebSocket connection when the component unmounts
     return () => {
-      // Close the WebSocket when the component unmounts
-      socket.close();
+      socket.disconnect();
     };
-  }, [accessToken]);
+  }, []);
+
+  console.log(notifications);
+  // useEffect(() => {
+  //   const socket = io('/.netlify/functions/fitbit-notifications'); // Replace with your server URL
+
+  //   // Event listener for notifications received over WebSocket
+  //   socket.on('notification', (data) => {
+  //     // Handle the incoming notification data in your app
+  //     console.log('Received Notification:', data);
+  //   });
+
+  //   return () => {
+  //     // Clean up the WebSocket connection when unmounting the component
+  //     socket.disconnect();
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   if (accessToken === null) return;
+
+  //   // Create a new WebSocket connection to the Fitbit Real-Time API
+  //   const socket = new WebSocket('wss://api.fitbit.com/1/user/-/bp/live');
+
+  //   // Set up the event handlers for the WebSocket connection
+  //   socket.onopen = () => {
+  //     // Connection is open; you can now subscribe to heart rate data.
+  //     // You will need to send an authorization header with your access token.
+
+  //     // Prepare the subscription request, including the access token
+  //     const subscriptionRequest = {
+  //       action: 'SUBSCRIBE',
+  //       device: 'tracker',
+  //       type: 'heartrate',
+  //       access_token: accessToken,
+  //     };
+
+  //     // Send the subscription request as a JSON string
+  //     socket.send(JSON.stringify(subscriptionRequest));
+  //   };
+
+  //   socket.onmessage = (event) => {
+  //     // Handle real-time heart rate data received in the event.data
+  //     const heartRateData = JSON.parse(event.data);
+  //     console.log('Live Heart Rate Data:', heartRateData);
+  //   };
+
+  //   socket.onclose = (event) => {
+  //     console.log('Socket is closed', event);
+  //     // Handle WebSocket closure (e.g., reconnection logic, error handling)
+  //   };
+
+  //   return () => {
+  //     // Close the WebSocket when the component unmounts
+  //     socket.close();
+  //   };
+  // }, [accessToken]);
 
   return (
     <div className="dark:bg-slate-800">
