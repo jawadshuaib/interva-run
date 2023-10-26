a93fb91e283a1bfd84d1bd5490e500ab9fe1ec6d
Jawad <jawad.php@gmail.com>
2023-10-26 01:26:41 -0400

    Switched back

--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -9,8 +9,8 @@ export default function Dashboard() {
   const userId = localStorage.getItem('userId');
 
   React.useEffect(() => {
-    // const apiUrl = `https://api.fitbit.com/1/user/${userId}/activities/heart/date/today/1m.json`;
-    const apiUrl = `https://api.fitbit.com/1/user/${userId}/hrv/date/2016-08-15.json`;
+    const apiUrl = `https://api.fitbit.com/1/user/${userId}/activities/heart/date/2016-08-14/1m.json`;
+    // const apiUrl = `https://api.fitbit.com/1/user/${userId}/hrv/date/2016-08-15.json`;
 
     const headers = {
       Authorization: `Bearer ${accessToken}`,
