c62b90909c506f5164065ffb30a5a903495b49a5
Jawad <jawad.php@gmail.com>
2023-10-25 23:29:57 -0400

    -

--- netlify/functions/fitbit-notifications.js
+++ netlify/functions/fitbit-notifications.js
@@ -1,47 +1,47 @@
 /* eslint-env node */
-import http from 'http';
+// import http from 'http';
 import WebSocket from 'ws';
 
-const server = http.createServer((req, res) => {
-  console.log('TEST 1');
-  if (req.method === 'POST') {
-    let data = '';
+// const server = http.createServer((req, res) => {
+//   console.log('TEST 1');
+//   if (req.method === 'POST') {
+//     let data = '';
 
-    console.log('TEST 2');
+//     console.log('TEST 2');
 
-    req.on('data', (chunk) => {
-      data += chunk;
-    });
+//     req.on('data', (chunk) => {
+//       data += chunk;
+//     });
 
-    req.on('end', () => {
-      try {
-        console.log('TEST 3');
-        const requestData = JSON.parse(data);
+//     req.on('end', () => {
+//       try {
+//         console.log('TEST 3');
+//         const requestData = JSON.parse(data);
 
-        // Handle and process the Fitbit notifications here
-        console.log('Received Fitbit Notifications:', requestData);
+//         // Handle and process the Fitbit notifications here
+//         console.log('Received Fitbit Notifications:', requestData);
 
-        // Broadcast the notification to connected WebSocket clients
-        wss.clients.forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
-            // Emit the 'notification' event to send notifications to clients
-            client.emit('notification', requestData);
-          }
-        });
+//         // Broadcast the notification to connected WebSocket clients
+//         wss.clients.forEach((client) => {
+//           if (client.readyState === WebSocket.OPEN) {
+//             // Emit the 'notification' event to send notifications to clients
+//             client.emit('notification', requestData);
+//           }
+//         });
 
-        res.writeHead(204);
-        res.end();
-      } catch (error) {
-        console.error('Error processing Fitbit notification:', error);
-        res.writeHead(500);
-        res.end('Internal Server Error');
-      }
-    });
-  } else {
-    res.writeHead(405); // Method Not Allowed
-    res.end('Method Not Allowed');
-  }
-});
+//         res.writeHead(204);
+//         res.end();
+//       } catch (error) {
+//         console.error('Error processing Fitbit notification:', error);
+//         res.writeHead(500);
+//         res.end('Internal Server Error');
+//       }
+//     });
+//   } else {
+//     res.writeHead(405); // Method Not Allowed
+//     res.end('Method Not Allowed');
+//   }
+// });
 
 const wss = new WebSocket.Server({ server });
 
@@ -49,34 +49,34 @@ server.listen(8080, () => {
   console.log('Server is listening on port 8080');
 });
 
-// wss.on('connection', (ws) => {
-//   // Handle WebSocket connection if needed
-//   ws.on('message', (message) => {
-//     // Handle WebSocket messages if needed
-//   });
-// });
+wss.on('connection', (ws) => {
+  // Handle WebSocket connection if needed
+  ws.on('message', (message) => {
+    // Handle WebSocket messages if needed
+  });
+});
 
-// export async function handler(event) {
-//   if (event.httpMethod === 'POST') {
-//     const requestData = JSON.parse(event.body);
-//     // Handle and process the Fitbit notifications here
+export async function handler(event) {
+  if (event.httpMethod === 'POST') {
+    const requestData = JSON.parse(event.body);
+    // Handle and process the Fitbit notifications here
 
-//     // Broadcast the notification to connected WebSocket clients
-//     wss.clients.forEach((client) => {
-//       if (client.readyState === WebSocket.OPEN) {
-//         client.send(JSON.stringify(requestData));
-//       }
-//     });
+    // Broadcast the notification to connected WebSocket clients
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(requestData));
+      }
+    });
 
-//     return {
-//       statusCode: 204,
-//     };
-//   } else {
-//     return {
-//       statusCode: 405, // Method Not Allowed
-//     };
-//   }
-// }
+    return {
+      statusCode: 204,
+    };
+  } else {
+    return {
+      statusCode: 405, // Method Not Allowed
+    };
+  }
+}
 
 // For verification
 // export async function handler(event) {
