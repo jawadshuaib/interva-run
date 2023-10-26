d96cb977ada3b6a245dc3d8d2e908cdf462c1fc1
Jawad <jawad.php@gmail.com>
2023-10-25 20:56:25 -0400

    Added verification code

--- netlify/functions/fitbit-notifications.js
+++ netlify/functions/fitbit-notifications.js
@@ -1,31 +1,21 @@
 /* eslint-env node */
-import fs from 'fs';
 
 export async function handler(event) {
-  // Parse the JSON data from the incoming request
-  const requestData = JSON.parse(event.body);
-  const updates = requestData.updates;
+  const queryParams = event.queryStringParameters;
+  const verifyCode = queryParams.verify;
 
-  // Save the updates to a text file
-  const updatesText = JSON.stringify(updates, null, 2);
-  fs.writeFileSync('fitbit-updates.txt', updatesText);
-
-  // Respond with HTTP 204 No Content
-  return {
-    statusCode: 204,
-  };
+  if (
+    verifyCode ===
+    '171ce6758e342278717f95a2034dde6cc4c1a584351b1be329adbe3cfd736039'
+  ) {
+    // Respond with HTTP 204 for the correct verification code
+    return {
+      statusCode: 204,
+    };
+  } else {
+    // Respond with HTTP 404 for an incorrect verification code
+    return {
+      statusCode: 404,
+    };
+  }
 }
-
-// export async function handler(event) {
-//   // Parse the JSON data from the incoming request
-//   const requestData = JSON.parse(event.body);
-//   const updates = requestData.updates;
-
-//   // Handle and process the Fitbit notifications here
-//   console.log('Received Fitbit Notifications:', updates);
-
-//   // Respond with HTTP 204 No Content
-//   return {
-//     statusCode: 204,
-//   };
-// }
